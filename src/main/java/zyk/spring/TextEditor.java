75870e5c902c0793cf0693993660fa182b7fe0ad
zhuoyikang <zhuoyikang@gmail.com>
2016-11-01 20:32:27 +0800

    not done

--- src/main/java/zyk/spring/TextEditor.java
+++ src/main/java/zyk/spring/TextEditor.java
@@ -3,11 +3,13 @@ package zyk.spring;
 public class TextEditor {
 
     private SpellChecker spellChecker;
-    public TextEditor(SpellChecker spellChecker){
+    public TextEditor(SpellChecker spellChecker) {
         System.out.println("Inside TextEditor constructor." );
         this.spellChecker = spellChecker;
     }
+
     public void spellCheck(){
         spellChecker.checkSpelling();
     }
+
 }
--- src/main/java/zyk/spring/TryApplication.java
+++ src/main/java/zyk/spring/TryApplication.java
@@ -12,26 +12,6 @@ public class TryApplication {
 
         ApplicationContext context =
             new ClassPathXmlApplicationContext("Beans.xml");
-
-        StudentJDBCTemplate studentJDBCTemplate =
-            (StudentJDBCTemplate)context.getBean("studentJDBCTemplate");
-
-        System.out.println("------Records creation--------" );
-        studentJDBCTemplate.create("Zara", 11, 99, 2010);
-        studentJDBCTemplate.create("Nuha", 20, 97, 2010);
-        studentJDBCTemplate.create("Ayan", 25, 100, 2011);
-        System.out.println("------Listing all the records--------" );
-
-        List<StudentMarks> studentMarks = studentJDBCTemplate.listStudents();
-
-        for (StudentMarks record : studentMarks) {
-            System.out.print("ID : " + record.getId() );
-            System.out.print(", Name : " + record.getName() );
-            System.out.print(", Marks : " + record.getMarks());
-            System.out.print(", Year : " + record.getYear());
-            System.out.println(", Age : " + record.getAge());
-        }
-
     }
 
 }
