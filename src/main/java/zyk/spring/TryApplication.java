f2ed9ccb890dfb8405a309083a1732289d73288b
zhuoyikang <zhuoyikang@gmail.com>
2016-11-01 13:29:23 +0800

    DONE

--- src/main/java/zyk/spring/TryApplication.java
+++ src/main/java/zyk/spring/TryApplication.java
@@ -12,29 +12,26 @@ public class TryApplication {
 
         ApplicationContext context =
             new ClassPathXmlApplicationContext("Beans.xml");
+
         StudentJDBCTemplate studentJDBCTemplate =
             (StudentJDBCTemplate)context.getBean("studentJDBCTemplate");
 
-        System.out.println("------Records Creation--------" );
-        studentJDBCTemplate.create("Zara", 11);
-        studentJDBCTemplate.create("Nuha", 2);
-        studentJDBCTemplate.create("Ayan", 15);
-        System.out.println("------Listing Multiple Records--------" );
+        System.out.println("------Records creation--------" );
+        studentJDBCTemplate.create("Zara", 11, 99, 2010);
+        studentJDBCTemplate.create("Nuha", 20, 97, 2010);
+        studentJDBCTemplate.create("Ayan", 25, 100, 2011);
+        System.out.println("------Listing all the records--------" );
 
-        List<Student> students = studentJDBCTemplate.listStudents();
-        for (Student record : students) {
+        List<StudentMarks> studentMarks = studentJDBCTemplate.listStudents();
+
+        for (StudentMarks record : studentMarks) {
             System.out.print("ID : " + record.getId() );
             System.out.print(", Name : " + record.getName() );
+            System.out.print(", Marks : " + record.getMarks());
+            System.out.print(", Year : " + record.getYear());
             System.out.println(", Age : " + record.getAge());
         }
 
-        System.out.println("----Updating Record with ID = 2 -----" );
-        studentJDBCTemplate.update(2, 20);
-        System.out.println("----Listing Record with ID = 2 -----" );
-        Student student = studentJDBCTemplate.getStudent(2);
-        System.out.print("ID : " + student.getId() );
-        System.out.print(", Name : " + student.getName() );
-        System.out.println(", Age : " + student.getAge());
     }
 
 }
