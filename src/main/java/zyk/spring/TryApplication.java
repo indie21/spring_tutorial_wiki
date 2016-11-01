61aae557977b3a5da3fa7fcf6747a5033f04e11a
zhuoyikang <zhuoyikang@gmail.com>
2016-11-01 10:51:06 +0800

    donw

--- src/main/java/zyk/spring/TryApplication.java
+++ src/main/java/zyk/spring/TryApplication.java
@@ -1,5 +1,6 @@
 package zyk.spring;
 
+import java.util.List;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -11,12 +12,29 @@ public class TryApplication {
 
         ApplicationContext context =
             new ClassPathXmlApplicationContext("Beans.xml");
+        StudentJDBCTemplate studentJDBCTemplate =
+            (StudentJDBCTemplate)context.getBean("studentJDBCTemplate");
 
-        Student student = (Student) context.getBean("student");
-        student.getName();
-        student.getAge();
-        student.printThrowException();
+        System.out.println("------Records Creation--------" );
+        studentJDBCTemplate.create("Zara", 11);
+        studentJDBCTemplate.create("Nuha", 2);
+        studentJDBCTemplate.create("Ayan", 15);
+        System.out.println("------Listing Multiple Records--------" );
 
+        List<Student> students = studentJDBCTemplate.listStudents();
+        for (Student record : students) {
+            System.out.print("ID : " + record.getId() );
+            System.out.print(", Name : " + record.getName() );
+            System.out.println(", Age : " + record.getAge());
+        }
+
+        System.out.println("----Updating Record with ID = 2 -----" );
+        studentJDBCTemplate.update(2, 20);
+        System.out.println("----Listing Record with ID = 2 -----" );
+        Student student = studentJDBCTemplate.getStudent(2);
+        System.out.print("ID : " + student.getId() );
+        System.out.print(", Name : " + student.getName() );
+        System.out.println(", Age : " + student.getAge());
     }
 
 }
