1c0fb1b26890ece74fe9688fb44c78d448fd4909
zhuoyikang <zhuoyikang@gmail.com>
2016-11-01 11:02:26 +0800

    DONE

--- src/main/java/zyk/spring/StudentJDBCTemplate.java
+++ src/main/java/zyk/spring/StudentJDBCTemplate.java
@@ -1,17 +1,24 @@
 package zyk.spring;
 
 import java.util.List;
+import java.util.Map;
 import javax.sql.DataSource;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcCall;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 
 public class StudentJDBCTemplate implements StudentDao {
 
     private DataSource dataSource;
     private JdbcTemplate jdbcTemplateObject;
+    private SimpleJdbcCall jdbcCall;
 
     public void setDataSource(DataSource dataSource) {
         this.dataSource = dataSource;
         this.jdbcTemplateObject = new JdbcTemplate(dataSource);
+        this.jdbcCall = new SimpleJdbcCall(dataSource).
+            withProcedureName("getRecord");
     }
 
     public void create(String name, Integer age) {
@@ -23,9 +30,19 @@ public class StudentJDBCTemplate implements StudentDao {
 
     public Student getStudent(Integer id) {
         String SQL = "select * from Student where id = ?";
-        Student student = jdbcTemplateObject.queryForObject(SQL,
-                                                            new Object[]{id},
-                                                            new StudentMapper());
+        // Student student = jdbcTemplateObject.queryForObject(SQL,
+        //                                                     new Object[]{id},
+        //                                                     new StudentMapper());
+        // return student;
+
+        SqlParameterSource in = new MapSqlParameterSource().
+            addValue("in_id", id);
+        Map<String, Object> out = jdbcCall.execute(in);
+        Student student = new Student();
+        student.setId(id);
+        student.setName((String) out.get("out_name"));
+        student.setAge((Integer) out.get("out_age"));
+
         return student;
     }
 
